6c9797fd7f079cd7d8abbaf292b4f84624340d07
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-15 13:49:10 -0400

    Crea el use case para obtener los usuarios

---
+++ app/src/main/java/edu/ucne/InsurePal/domain/useCases/obtenerUsuariosUseCase.kt
@@ -0,0 +1,13 @@
+package edu.ucne.InsurePal.domain.useCases
+
+import edu.ucne.InsurePal.data.Resource
+import edu.ucne.InsurePal.domain.Usuario
+import edu.ucne.InsurePal.domain.UsuarioRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class obtenerUsuariosUseCase @Inject constructor(
+    private val repository: UsuarioRepository
+) {
+    suspend operator fun invoke(): Flow<Resource<List<Usuario>>> = repository.getUsuarios()
+}
