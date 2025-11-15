655a5cf5417b411bf5bb7188d7d06b285d3229de
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-15 13:48:56 -0400

    Crea el caso de uso para guardar usuarios

---
+++ app/src/main/java/edu/ucne/InsurePal/domain/useCases/saveUsuarioUseCase.kt
@@ -0,0 +1,34 @@
+package edu.ucne.InsurePal.domain.useCases
+
+import edu.ucne.InsurePal.data.Resource
+import edu.ucne.InsurePal.data.toRequest
+import edu.ucne.InsurePal.domain.Usuario
+import edu.ucne.InsurePal.domain.UsuarioRepository
+import javax.inject.Inject
+
+class saveUsuarioUseCase @Inject constructor(
+    private val repository: UsuarioRepository
+) {
+    suspend operator fun invoke(id: Int = 0, usuario: Usuario): Resource<Usuario?> {
+
+        val usuarioRequest = usuario.toRequest()
+        val result: Resource<Usuario?> = if (id == 0) {
+            val postResult = repository.postUsuario(usuarioRequest)
+            when (postResult) {
+                is Resource.Success -> Resource.Success(postResult.data)
+                is Resource.Error -> Resource.Error(postResult.message ?: "Error", postResult.data)
+                is Resource.Loading -> Resource.Loading(postResult.data)
+            }
+
+        } else {
+            val putResult = repository.putUsuario(id, usuarioRequest)
+            when (putResult) {
+                is Resource.Success -> Resource.Success(usuario)
+                is Resource.Error -> Resource.Error(putResult.message ?: "Error al actualizar")
+                is Resource.Loading -> Resource.Loading()
+            }
+        }
+
+        return result
+    }
+}
