99f0371f0ef4dcc3354e75e6a97874bc86a85483
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-16 14:54:29 -0400

    Agrega eventos para el registro de usuarios

--- app/src/main/java/edu/ucne/InsurePal/presentation/usuario/UsuarioEvent.kt
+++ app/src/main/java/edu/ucne/InsurePal/presentation/usuario/UsuarioEvent.kt
@@ -1,4 +1,4 @@
-package edu.ucne.InsurePal.presentation
+package edu.ucne.InsurePal.presentation.usuario
 
 import edu.ucne.InsurePal.domain.Usuario
 
@@ -10,10 +10,16 @@ sealed interface UsuarioEvent
     data class obtener(val id: Int?): UsuarioEvent
     object userMessageShown : UsuarioEvent
 
-
     data class crear(val usuario: Usuario): UsuarioEvent
     data class actualizar(val usuario: Usuario) : UsuarioEvent
 
     data object new: UsuarioEvent
     data object onLoginClick : UsuarioEvent
+
+    data object registerNewUser : UsuarioEvent
+    data object showRegistrationDialog : UsuarioEvent
+    data object hideRegistrationDialog : UsuarioEvent
+    data class onRegUsernameChange(val userName: String) : UsuarioEvent
+    data class onRegPasswordChange(val password: String) : UsuarioEvent
+    data class onRegConfirmPasswordChange(val password: String) : UsuarioEvent
 }
