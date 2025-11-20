7517f124905c15b957d985d7fcdfcb708511f75f
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-20 18:19:53 -0400

    Crea el modelo SeguroVehiculo

---
+++ app/src/main/java/edu/ucne/InsurePal/domain/polizas/vehiculo/model/SeguroVehiculo.kt
@@ -0,0 +1,14 @@
+package edu.ucne.InsurePal.domain.polizas.vehiculo.model
+
+import java.time.LocalDate
+
+class SeguroVehiculo (
+    val idPoliza: String? = null,
+    val name: String,
+    val status: String,
+    val expirationDate: LocalDate,
+
+    val placa: String,
+    val modeloVehiculo: String,
+    val coverageType: String
+)
