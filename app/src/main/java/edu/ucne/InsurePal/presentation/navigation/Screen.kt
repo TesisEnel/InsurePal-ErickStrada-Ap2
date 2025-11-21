72aee16477b66ea22e62679e7d0d654429fd9170
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-21 12:57:18 -0400

    Agrega las rutas de navegación para seguros y vehículos

--- app/src/main/java/edu/ucne/InsurePal/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/InsurePal/presentation/navigation/Screen.kt
@@ -5,4 +5,11 @@ package edu.ucne.InsurePal.presentation.navigation
 sealed class Screen(val route: String) {
     object Login : Screen("login")
     object Home : Screen("home")
+
+    object SeleccionSeguro : Screen("seleccion_seguro")
+
+    object MisVehiculos : Screen("mis_vehiculos")
+
+    object VehiculoRegistro : Screen("vehiculo_registro")
+
 }
