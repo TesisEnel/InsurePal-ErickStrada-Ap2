3187b7ca7e6e2cfc0028d9260ff4c46108719fa4
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-23 20:03:07 -0400

    Agrega el evento `OnVolverClick` a `CotizacionVehiculoEvent`

--- app/src/main/java/edu/ucne/InsurePal/presentation/polizas/vehiculo/cotizacionVehiculo/CotizacionVehiculoEvent.kt
+++ app/src/main/java/edu/ucne/InsurePal/presentation/polizas/vehiculo/cotizacionVehiculo/CotizacionVehiculoEvent.kt
@@ -2,4 +2,5 @@ package edu.ucne.InsurePal.presentation.polizas.vehiculo.cotizacionVehiculo
 
 sealed interface CotizacionVehiculoEvent {
     data object OnContinuarPagoClick : CotizacionVehiculoEvent
+    data object OnVolverClick : CotizacionVehiculoEvent
 }
