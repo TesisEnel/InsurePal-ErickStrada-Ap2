084e9c4a6acab4ab0201e04dd6f7169bd9330fcb
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-27 12:20:43 -0400

    Modifica el caso de uso `ObtenerMarcasUseCase` para que sea suspendido y devuelva un `Resource`

--- app/src/main/java/edu/ucne/InsurePal/domain/polizas/vehiculo/useCases/ObtenerMarcasUseCase.kt
+++ app/src/main/java/edu/ucne/InsurePal/domain/polizas/vehiculo/useCases/ObtenerMarcasUseCase.kt
@@ -1,13 +1,14 @@
 package edu.ucne.InsurePal.domain.polizas.vehiculo.useCases
 
+import edu.ucne.InsurePal.data.Resource
 import edu.ucne.InsurePal.domain.polizas.vehiculo.model.MarcaVehiculo
-import edu.ucne.InsurePal.domain.polizas.vehiculo.repository.VehiculoRepository
+import edu.ucne.InsurePal.domain.polizas.vehiculo.repository.SeguroVehiculoRepository
 import jakarta.inject.Inject
 
 class ObtenerMarcasUseCase @Inject constructor(
-    private val repository: VehiculoRepository
+    private val repository: SeguroVehiculoRepository
 ) {
-    operator fun invoke(): List<MarcaVehiculo> {
+    suspend operator fun invoke(): Resource<List<MarcaVehiculo>> {
         return repository.getMarcas()
     }
 }
