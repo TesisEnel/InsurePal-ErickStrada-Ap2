d851985f3152cde86b5c720fd9231c7d38785dd8
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-27 12:21:13 -0400

    Elimina la implementación de `VehiculoRepository`

--- app/src/main/java/edu/ucne/InsurePal/domain/polizas/vehiculo/repository/VehiculoRepository.kt
+++
@@ -1,8 +0,0 @@
-package edu.ucne.InsurePal.domain.polizas.vehiculo.repository
-
-import edu.ucne.InsurePal.domain.polizas.vehiculo.model.MarcaVehiculo
-
-interface VehiculoRepository {
-    fun getMarcas(): List<MarcaVehiculo>
-    fun getPrecioBase(marca: String, modelo: String): Double?
-}
