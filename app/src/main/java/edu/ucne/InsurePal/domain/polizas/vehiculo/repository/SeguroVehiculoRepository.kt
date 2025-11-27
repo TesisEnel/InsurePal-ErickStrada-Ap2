b53b262dab2759c3d8d673f757793a3a3e477a53
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-27 12:21:06 -0400

    Agrega el método `getMarcas` a la interfaz `SeguroVehiculoRepository`

--- app/src/main/java/edu/ucne/InsurePal/domain/polizas/vehiculo/repository/SeguroVehiculoRepository.kt
+++ app/src/main/java/edu/ucne/InsurePal/domain/polizas/vehiculo/repository/SeguroVehiculoRepository.kt
@@ -2,13 +2,12 @@ package edu.ucne.InsurePal.domain.polizas.vehiculo.repository
 
 import edu.ucne.InsurePal.data.Resource
 import edu.ucne.InsurePal.data.remote.polizas.vehiculo.dto.SeguroVehiculoRequest
+import edu.ucne.InsurePal.domain.polizas.vehiculo.model.MarcaVehiculo
 import edu.ucne.InsurePal.domain.polizas.vehiculo.model.SeguroVehiculo
 import edu.ucne.InsurePal.domain.polizas.vida.model.SeguroVida
 import kotlinx.coroutines.flow.Flow
 
 interface SeguroVehiculoRepository {
-
-
     fun getVehiculos(usuarioId: Int): Flow<Resource<List<SeguroVehiculo>>>
 
     suspend fun getVehiculo(id: String): Resource<SeguroVehiculo>
@@ -18,4 +17,6 @@ interface SeguroVehiculoRepository {
     suspend fun putVehiculo(id: String, seguro: SeguroVehiculo): Resource<Unit>
 
     suspend fun delete(id: String) : Resource<Unit>
+
+    suspend fun getMarcas(): Resource<List<MarcaVehiculo>>
 }
