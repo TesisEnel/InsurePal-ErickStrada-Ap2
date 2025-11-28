0ead26d5b07386e8482c46dc31b810a96db331d1
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-28 17:30:45 -0400

    Crea el data class ReclamoResponse

---
+++ app/src/main/java/edu/ucne/InsurePal/data/remote/reclamoVehiculo/ReclamoResponse.kt
@@ -0,0 +1,29 @@
+package edu.ucne.InsurePal.data.remote.reclamoVehiculo
+
+data class ReclamoResponse(
+    val id: String,
+
+    val folio: String,
+
+    val polizaId: String,
+
+    val usuarioId: Int,
+
+    val descripcion: String,
+
+    val direccion: String,
+
+    val tipoIncidente: String,
+
+    val fechaIncidente: String,
+
+    val imagenUrl: String,
+
+    val status: String,
+
+    val motivoRechazo: String?,
+
+    val fechaCreacion: String,
+
+    val fechaActualizacion: String?
+)
