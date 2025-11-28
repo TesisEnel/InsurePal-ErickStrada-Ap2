3cd2965c924f7e0481cb35574763e3f10a3f45c2
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-28 17:37:41 -0400

    Crea la implementación de ReclamoVehiculoRepository

--- app/src/main/java/edu/ucne/InsurePal/di/RepositoryModule.kt
+++ app/src/main/java/edu/ucne/InsurePal/di/RepositoryModule.kt
@@ -7,10 +7,12 @@ import dagger.hilt.components.SingletonComponent
 import edu.ucne.InsurePal.data.local.pago.PagoRepositoryImpl
 import edu.ucne.InsurePal.data.remote.polizas.vehiculo.SeguroVehiculoRepositoryImpl
 import edu.ucne.InsurePal.data.remote.polizas.vida.SeguroVidaRepositoryImpl
+import edu.ucne.InsurePal.data.remote.reclamoVehiculo.ReclamoVehiculoRepositoryImpl
 import edu.ucne.InsurePal.data.remote.usuario.UsuarioRepositoryImpl
 import edu.ucne.InsurePal.domain.pago.repository.PagoRepository
 import edu.ucne.InsurePal.domain.polizas.vehiculo.repository.SeguroVehiculoRepository
 import edu.ucne.InsurePal.domain.polizas.vida.repository.SeguroVidaRepository
+import edu.ucne.InsurePal.domain.reclamoVehiculo.repository.ReclamoVehiculoRepository
 import edu.ucne.InsurePal.domain.usuario.repository.UsuarioRepository
 import javax.inject.Singleton
 
@@ -34,6 +36,12 @@ abstract class RepositoryModule {
     abstract fun bindPagoRepository(
         pagoRepositoryImpl: PagoRepositoryImpl
     ): PagoRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindReclamoVehiculoRepository(
+        reclamoVehiculoRepositoryImpl: ReclamoVehiculoRepositoryImpl
+    ): ReclamoVehiculoRepository
 
     @Binds
     @Singleton
