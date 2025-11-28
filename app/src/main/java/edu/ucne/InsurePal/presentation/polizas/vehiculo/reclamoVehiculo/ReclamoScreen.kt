241657d8de0de0ec6e4dd6fe660cade19e660b54
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-28 18:41:04 -0400

    Refactor: Elimina el import no utilizado `KeyboardType`

--- app/src/main/java/edu/ucne/InsurePal/presentation/polizas/vehiculo/reclamoVehiculo/ReclamoScreen.kt
+++ app/src/main/java/edu/ucne/InsurePal/presentation/polizas/vehiculo/reclamoVehiculo/ReclamoScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
