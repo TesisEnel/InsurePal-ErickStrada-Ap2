67dad328b62a2ee23dad3ff4c58bbb9eb85dd15f
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-29 13:37:02 -0400

    Crea el DTO ReclamoVidaResponse

---
+++ app/src/main/java/edu/ucne/InsurePal/data/remote/reclamoVida/dto/ReclamoVidaCreateRequest.kt
@@ -0,0 +1,34 @@
+package edu.ucne.InsurePal.data.remote.reclamoVida.dto
+
+data class ReclamoVidaResponse(
+
+    val id: String,
+
+    val folio: String?,
+
+    val polizaId: String,
+
+    val usuarioId: Int,
+
+    val nombreAsegurado: String,
+
+    val descripcion: String,
+
+    val lugarFallecimiento: String,
+
+    val causaMuerte: String,
+
+    val fechaFallecimiento: String,
+
+    val numCuenta: String?,
+
+    val actaDefuncionUrl: String?,
+
+    val identificacionUrl: String?,
+
+    val status: String,
+
+    val motivoRechazo: String?,
+
+    val fechaCreacion: String
+)
