6110748d46db95cd68cec5e7882ebc7b5dd9f66b
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-28 21:30:36 -0400

    Crea el UiState DetalleReclamoUiState

---
+++ app/src/main/java/edu/ucne/InsurePal/presentation/listaReclamos/detalleReclamo/DetalleReclamoUiState.kt
@@ -0,0 +1,12 @@
+package edu.ucne.InsurePal.presentation.listaReclamos.detalleReclamo
+
+import edu.ucne.InsurePal.domain.reclamoVehiculo.model.ReclamoVehiculo
+import edu.ucne.InsurePal.presentation.listaReclamos.UiModels.TipoReclamo
+
+data class DetalleReclamoUiState(
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val tipo: TipoReclamo = TipoReclamo.VEHICULO,
+    val reclamoVehiculo: ReclamoVehiculo? = null
+    // val reclamoVida: ReclamoVida? = null // TODO: Agregar cuando se cree la entidad ReclamoVida
+)
