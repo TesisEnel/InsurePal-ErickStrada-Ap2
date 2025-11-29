90036132c61fd50debfb240d90fb486b69d63c03
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-29 16:31:33 -0400

    Habilita la propiedad `reclamoVida` en `DetalleReclamoUiState`

--- app/src/main/java/edu/ucne/InsurePal/presentation/listaReclamos/detalleReclamo/DetalleReclamoUiState.kt
+++ app/src/main/java/edu/ucne/InsurePal/presentation/listaReclamos/detalleReclamo/DetalleReclamoUiState.kt
@@ -1,6 +1,7 @@
 package edu.ucne.InsurePal.presentation.listaReclamos.detalleReclamo
 
 import edu.ucne.InsurePal.domain.reclamoVehiculo.model.ReclamoVehiculo
+import edu.ucne.InsurePal.domain.reclamoVida.model.ReclamoVida
 import edu.ucne.InsurePal.presentation.listaReclamos.UiModels.TipoReclamo
 
 data class DetalleReclamoUiState(
@@ -9,6 +10,6 @@ data class DetalleReclamoUiState(
     val exitoOperacion: String? = null,
     val error: String? = null,
     val tipo: TipoReclamo = TipoReclamo.VEHICULO,
-    val reclamoVehiculo: ReclamoVehiculo? = null
-    // val reclamoVida: ReclamoVida? = null // TODO: Agregar cuando se cree la entidad ReclamoVida
+    val reclamoVehiculo: ReclamoVehiculo? = null,
+     val reclamoVida: ReclamoVida? = null
 )
