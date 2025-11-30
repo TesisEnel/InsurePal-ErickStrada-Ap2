faa15d5c75ff5c91864e4d9aa3cedca9675c3b59
ErickEstradaR <erickestradarosario@gmail.com>
2025-11-30 12:17:21 -0400

    Refactor: Encapsula los parámetros de `crearReclamoVida` en `CrearReclamoVidaParams`
    Se ha modificado la función `crearReclamoVida` para recibir un único objeto de parámetros `CrearReclamoVidaParams` en lugar de múltiples argumentos individuales.

--- app/src/main/java/edu/ucne/InsurePal/domain/reclamoVida/repository/ReclamoVidaRepository.kt
+++ app/src/main/java/edu/ucne/InsurePal/domain/reclamoVida/repository/ReclamoVidaRepository.kt
@@ -1,21 +1,15 @@
 package edu.ucne.InsurePal.domain.reclamoVida.repository
 
 import edu.ucne.InsurePal.data.Resource
+import edu.ucne.InsurePal.domain.reclamoVida.model.CrearReclamoVidaParams
 import edu.ucne.InsurePal.domain.reclamoVida.model.ReclamoVida
 import java.io.File
 
 interface ReclamoVidaRepository {
-    suspend fun crearReclamoVida(
-        polizaId: String,
-        usuarioId: Int,
-        nombreAsegurado: String,
-        descripcion: String,
-        lugarFallecimiento: String,
-        causaMuerte: String,
-        fechaFallecimiento: String,
-        numCuenta: String,
-        actaDefuncion: File
-    ): Resource<ReclamoVida>
+
+
+    suspend fun crearReclamoVida(params: CrearReclamoVidaParams): Resource<ReclamoVida>
+
 
     suspend fun obtenerReclamosVida(
         usuarioId: Int? = null
