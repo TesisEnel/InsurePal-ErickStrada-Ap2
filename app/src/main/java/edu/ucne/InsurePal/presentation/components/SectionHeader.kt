972eb3c3293093dc7ebde241b9cee836c3541d69
ErickEstradaR <erickestradarosario@gmail.com>
2025-12-02 16:38:07 -0400

    Crea el `SectionHeader` composable

---
+++ app/src/main/java/edu/ucne/InsurePal/presentation/components/SectionHeader.kt
@@ -0,0 +1,20 @@
+package edu.ucne.InsurePal.presentation.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SectionHeader(title: String, modifier: Modifier = Modifier) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.titleMedium,
+        color = MaterialTheme.colorScheme.primary,
+        fontWeight = FontWeight.SemiBold,
+        modifier = modifier.padding(vertical = 8.dp)
+    )
+}
